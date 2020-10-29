a119b6b57c26b42695556939604687359e64dd0c
wdammak <26695620+wdammak@users.noreply.github.com>
2020-10-29 12:24:32 +0100

    Update Product.php

--- classes/Product.php
+++ classes/Product.php
@@ -2369,7 +2369,7 @@ class ProductCore extends ObjectModel
 
         $id_address = $context->cart->{Configuration::get('PS_TAX_ADDRESS_TYPE')};
         $ids = Address::getCountryAndState($id_address);
-	$ids['id_country'] = $ids['id_country'] ?? null;
+	$ids['id_country'] = (int) $ids['id_country'] : (int) Configuration::get('PS_COUNTRY_DEFAULT');
         $id_country = $ids['id_country'] ? (int)$ids['id_country'] : (int)Configuration::get('PS_COUNTRY_DEFAULT');
 
         return SpecificPrice::getProductIdByDate(
@@ -3462,7 +3462,7 @@ class ProductCore extends ObjectModel
 			JOIN `'._DB_PREFIX_.'attribute` a ON (a.`id_attribute` = pac.`id_attribute`)
 			JOIN `'._DB_PREFIX_.'attribute_lang` al ON (a.`id_attribute` = al.`id_attribute` AND al.`id_lang` = '.(int)$id_lang.')
 			JOIN `'._DB_PREFIX_.'attribute_group` ag ON (a.id_attribute_group = ag.`id_attribute_group`)
-			WHERE pa.`id_product` IN ('.implode(array_map('intval', $products), ',').') AND ag.`is_color_group` = 1
+			WHERE pa.`id_product` IN ('.implode(',', array_map('intval', $products)).') AND ag.`is_color_group` = 1
 			GROUP BY pa.`id_product`, a.`id_attribute`, `group_by`
 			'.($check_stock ? 'HAVING qty > 0' : '').'
 			ORDER BY a.`position` ASC;'
@@ -3739,7 +3739,7 @@ class ProductCore extends ObjectModel
 		LEFT JOIN '._DB_PREFIX_.'feature_value_lang fvl ON (fvl.id_feature_value = pf.id_feature_value AND fvl.id_lang = '.(int)$id_lang.')
 		LEFT JOIN '._DB_PREFIX_.'feature f ON (f.id_feature = pf.id_feature)
 		'.Shop::addSqlAssociation('feature', 'f').'
-		WHERE `id_product` IN ('.implode($product_implode, ',').')
+		WHERE `id_product` IN ('.implode(',', $product_implode).')
 		ORDER BY f.position ASC');
 
         foreach ($result as $row) {
