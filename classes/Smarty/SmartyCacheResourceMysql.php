ca8829cb58cb316b021248531956a1cde2e42dfb
shreesh arora <shreesh.arora608@webkul.com>
2021-05-03 14:00:16 +0530

    Fix smarty class path

--- classes/Smarty/SmartyCacheResourceMysql.php
+++ classes/Smarty/SmartyCacheResourceMysql.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2015 PrestaShop
+ * 2007-2017 PrestaShop
  *
  * NOTICE OF LICENSE
  *
@@ -19,12 +19,12 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2015 PrestaShop SA
+ * @copyright 2007-2017 PrestaShop SA
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
 
-require_once(_PS_VENDOR_DIR_.'/prestashop/smarty/Smarty.class.php');
+require_once(_PS_TOOL_DIR_.'smarty/Smarty.class.php');
 
 class Smarty_CacheResource_Mysql extends Smarty_CacheResource_Custom
 {
