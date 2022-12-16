shreesh-webkul/hotelcommerce

Added feature occupancy wise search

modules/blockcart/controllers/front/checkroomavailabilityajaxprocess.php
@@ -31,20 +31,16 @@ class BlockcartCheckRoomAvailabilityAjaxProcessModuleFrontController extends Mod
                 )) {
                     if ($roomTypeInfo = $objRoomType->getRoomTypeInfoByIdProduct($idProduct)) {
                         if ($idHotel = $roomTypeInfo['id_hotel']) {
-                            if ($hotelRoomData = $objBookingDetail->DataForFrontSearch(
-                                $dateFrom,
-                                $dateTo,
-                                $idHotel,
-                                $idProduct,
-                                1,
-                                0,
-                                0,
-                                -1,
-                                0,
-                                0,
-                                $idCart,
-                                $idGuest
-                            )) {
+                            $booking_params = array(
+                                'date_from' => $dateFrom,
+                                'date_to' => $dateTo,
+                                'hotel_id' => $idHotel,
+                                'id_room_type' => $idProduct,
+                                'only_search_data' => 1,
+                                'id_cart' => $idCart,
+                                'id_guest' => $idGuest,
+                            );
+                            if ($hotelRoomData = $obj_booking_dtl->dataForFrontSearch($booking_params)) {
                                 $totalAvailRooms = $hotelRoomData['stats']['num_avail'];
                             }
                         }

Qloapps/QloApps

modules/blockcart/controllers/front/checkroomavailabilityajaxprocess.php
@@ -31,20 +31,16 @@ class BlockcartCheckRoomAvailabilityAjaxProcessModuleFrontController extends Mod
                 )) {
                     if ($roomTypeInfo = $objRoomType->getRoomTypeInfoByIdProduct($idProduct)) {
                         if ($idHotel = $roomTypeInfo['id_hotel']) {
-                            if ($hotelRoomData = $objBookingDetail->DataForFrontSearch(
-                                $dateFrom,
-                                $dateTo,
-                                $idHotel,
-                                $idProduct,
-                                1,
-                                0,
-                                0,
-                                -1,
-                                0,
-                                0,
-                                $idCart,
-                                $idGuest
-                            )) {
+                            $bookingParams = array(
+                                'date_from' => $dateFrom,
+                                'date_to' => $dateTo,
+                                'hotel_id' => $idHotel,
+                                'id_room_type' => $idProduct,
+                                'only_search_data' => 1,
+                                'id_cart' => $idCart,
+                                'id_guest' => $idGuest,
+                            );
+                            if ($hotelRoomData = $objBookingDetail->dataForFrontSearch($bookingParams)) {
                                 $totalAvailRooms = $hotelRoomData['stats']['num_avail'];
                             }
                         }