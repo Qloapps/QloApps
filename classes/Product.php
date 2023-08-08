80230795be90877f982e8ff5c188de0392e29459
Abhishek Kumar <abhishek1.kumar029@webkul.in>
2023-08-08 18:33:35 +0530

    Fix PHP warninh on search results page

--- classes/Product.php
+++ classes/Product.php
@@ -2579,8 +2579,12 @@ class ProductCore extends ObjectModel
         }
 
         $id_address = $context->cart->{Configuration::get('PS_TAX_ADDRESS_TYPE')};
+
+        $id_country = (int) Configuration::get('PS_COUNTRY_DEFAULT');
         $ids = Address::getCountryAndState($id_address);
-	$id_country = $ids['id_country'] ? (int) $ids['id_country'] : (int) Configuration::get('PS_COUNTRY_DEFAULT');
+        if (isset($ids['id_country']) && (int) $ids['id_country']) {
+            $id_country = (int) $ids['id_country'];
+        }
 
         return SpecificPrice::getProductIdByDate(
             $context->shop->id,
