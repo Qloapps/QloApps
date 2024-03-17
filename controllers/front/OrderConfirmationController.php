993b41277e5f965e5332ebd81dfa36cfb3db9360
Shreesh Arora <shreesh.arora608@webkul.in>
2024-03-17 13:01:53 +0530

    resolve all room not showing in order confirmation page

--- controllers/front/OrderConfirmationController.php
+++ controllers/front/OrderConfirmationController.php
@@ -155,9 +155,6 @@ class OrderConfirmationControllerCore extends FrontController
 
                     if (!empty($orderProducts)) {
                         foreach ($orderProducts as $type_key => $type_value) {
-                            if (in_array($type_value['product_id'], $processed_product)) {
-                                continue;
-                            }
                             $processed_product[] = $type_value['product_id'];
 
                             $product = new Product($type_value['product_id'], false, $this->context->language->id);
@@ -372,13 +369,6 @@ class OrderConfirmationControllerCore extends FrontController
 
                             }
                         }
-
-                        if (!empty($cart_htl_data)) {
-                            $this->context->smarty->assign('cart_htl_data', $cart_htl_data);
-                        }
-                        if (!empty($cart_service_products)) {
-                            $this->context->smarty->assign('cart_service_products', $cart_service_products);
-                        }
                     }
                     if (!$objCartOrder->hasInvoice()) {
                         $orders_has_invoice = 0;
@@ -400,6 +390,13 @@ class OrderConfirmationControllerCore extends FrontController
                     $orderTotalInfo['total_paid'] += $objCartOrder->total_paid;
                     $orderTotalInfo['total_paid_real'] += $objCartOrder->total_paid_real;
                 }
+            }
+
+            if (!empty($cart_htl_data)) {
+                $this->context->smarty->assign('cart_htl_data', $cart_htl_data);
+            }
+            if (!empty($cart_service_products)) {
+                $this->context->smarty->assign('cart_service_products', $cart_service_products);
             }
 
             $this->context->smarty->assign('orderTotalInfo', $orderTotalInfo);
