34ebe4691b37df7630c736567300e15a6c6251d7
Shreesh Arora <shreesh.arora608@webkul.in>
2024-03-26 16:52:29 +0530

    Fix: country validation were applied when creating order from back office

--- controllers/admin/AdminOrdersController.php
+++ controllers/admin/AdminOrdersController.php
@@ -1337,41 +1337,30 @@ class AdminOrdersControllerCore extends AdminController
                             $extraVars = null;
                         }
 
-                        $bad_delivery = false;
-                        if (($bad_delivery = (bool)!Address::isCountryActiveById((int)$objCart->id_address_delivery))
-                            || !Address::isCountryActiveById((int)$objCart->id_address_invoice)
-                        ) {
-                            if ($bad_delivery) {
-                                $this->errors[] = Tools::displayError('This booking address country is not active.');
-                            } else {
-                                $this->errors[] = Tools::displayError('This invoice address country is not active.');
-                            }
-                        } else {
-                            $amountPaid = Tools::ps_round($amountPaid, 6);
-                            $objEmployee = new Employee($this->context->cookie->id_employee);
+                        $amountPaid = Tools::ps_round($amountPaid, 6);
+                        $objEmployee = new Employee($this->context->cookie->id_employee);
 
-                            $objPaymentModule->validateOrder(
-                                $objCart->id,
-                                $idOrderState,
-                                $amountPaid,
-                                $objPaymentModule->displayName,
-                                $this->l('Manual order -- Employee:').' '.substr($objEmployee->firstname, 0, 1).'. '.$objEmployee->lastname,
-                                $extraVars,
-                                null,
-                                false,
-                                $objCart->secure_key
-                            );
+                        $objPaymentModule->validateOrder(
+                            $objCart->id,
+                            $idOrderState,
+                            $amountPaid,
+                            $objPaymentModule->displayName,
+                            $this->l('Manual order -- Employee:').' '.substr($objEmployee->firstname, 0, 1).'. '.$objEmployee->lastname,
+                            $extraVars,
+                            null,
+                            false,
+                            $objCart->secure_key
+                        );
 
-                            if (isset($this->context->cookie->id_cart)) {
-                                unset($this->context->cookie->id_cart);
-                            }
-                            if (isset($this->context->cookie->id_guest)) {
-                                unset($this->context->cookie->id_guest);
-                            }
+                        if (isset($this->context->cookie->id_cart)) {
+                            unset($this->context->cookie->id_cart);
+                        }
+                        if (isset($this->context->cookie->id_guest)) {
+                            unset($this->context->cookie->id_guest);
+                        }
 
-                            if ($objPaymentModule->currentOrder) {
-                                Tools::redirectAdmin(self::$currentIndex.'&id_order='.$objPaymentModule->currentOrder.'&vieworder'.'&token='.$this->token.'&conf=3');
-                            }
+                        if ($objPaymentModule->currentOrder) {
+                            Tools::redirectAdmin(self::$currentIndex.'&id_order='.$objPaymentModule->currentOrder.'&vieworder'.'&token='.$this->token.'&conf=3');
                         }
                     } else {
                         // if errors render add order form
