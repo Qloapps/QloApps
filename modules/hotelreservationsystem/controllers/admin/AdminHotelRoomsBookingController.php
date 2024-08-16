60c0c4fddcf25f15869f77b7d625684a327b0a7b
Shreesh Arora <shreesh.arora608@webkul.in>
2024-08-16 12:03:52 +0530

    remove debug code

--- modules/hotelreservationsystem/controllers/admin/AdminHotelRoomsBookingController.php
+++ modules/hotelreservationsystem/controllers/admin/AdminHotelRoomsBookingController.php
@@ -855,7 +855,7 @@ class AdminHotelRoomsBookingController extends ModuleAdminController
                 }
             }
         }
-        // ddd($booking_data);
+
         return $booking_data;
     }
 
