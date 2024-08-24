7f484f4d5c58ff88a869428cff1267eef012bac7
wdammak <bestcomtechnologyci@gmail.com>
2024-08-24 01:21:35 +0100

    Fix Trying to access array offset on value of type bool
    PHP Notice:  Trying to access array offset on value of type bool

--- classes/shop/ShopUrl.php
+++ classes/shop/ShopUrl.php
@@ -167,8 +167,13 @@ class ShopUrlCore extends ObjectModel
 			FROM '._DB_PREFIX_.'shop_url
 			WHERE main = 1
 			AND id_shop = '.($id_shop !== null ? (int)$id_shop : (int)Context::getContext()->shop->id));
-            self::$main_domain[(int)$id_shop] = $row['domain'];
-            self::$main_domain_ssl[(int)$id_shop] = $row['domain_ssl'];
+            if ($row && is_array($row)) {
+                self::$main_domain[(int)$id_shop] = $row['domain'];
+                self::$main_domain_ssl[(int)$id_shop] = $row['domain_ssl'];
+            } else {
+                self::$main_domain[(int)$id_shop] = null;
+                self::$main_domain_ssl[(int)$id_shop] = null;
+            }
         }
     }
 
