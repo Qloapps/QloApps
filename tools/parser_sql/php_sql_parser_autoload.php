223d4286587d1d4ae663f09b84157da2d0b1cdaf
Amit sharma <amit.sharma322@webkul.in>
2025-12-30 15:43:35 +0530

    Fixed: remove text formatter

--- tools/parser_sql/php_sql_parser_autoload.php
+++ tools/parser_sql/php_sql_parser_autoload.php
@@ -1,37 +1,37 @@
 <?php
-
 /**
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License version 3.0
- * that is bundled with this package in the file LICENSE.md
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/license/osl-3-0-php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to support@qloapps.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade this module to a newer
- * versions in the future. If you wish to customize this module for your needs
- * please refer to https://store.webkul.com/customisation-guidelines for more information.
- *
- * @author Webkul IN
- * @copyright Since 2010 Webkul
- * @license https://opensource.org/license/osl-3-0-php Open Software License version 3.0
- */
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License version 3.0
+* that is bundled with this package in the file LICENSE.md
+* It is also available through the world-wide-web at this URL:
+* https://opensource.org/license/osl-3-0-php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to support@qloapps.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade this module to a newer
+* versions in the future. If you wish to customize this module for your needs
+* please refer to https://store.webkul.com/customisation-guidelines for more information.
+*
+* @author Webkul IN
+* @copyright Since 2010 Webkul
+* @license https://opensource.org/license/osl-3-0-php Open Software License version 3.0
+*/
 
-require_once __DIR__ . '/PHPSQLParser.php';
+require_once __DIR__.'/PHPSQLParser.php';
 
 function classAutoLoader($class)
 {
     // check if php sql parser file is called
-    if (0 !== strpos($class, 'PHPSQLParser')) {
+    if(0 !== strpos($class, 'PHPSQLParser')) {
         return;
     }
 
-    $className = end(explode('\\', $class));
+    $parts = explode('\\', $class);
+    $className = end($parts);
     if(class_exists($className)) {
         return;
     }
@@ -39,7 +39,7 @@ function classAutoLoader($class)
     $class = str_replace('PHPSQLParser\\', '', $class);
 
     // create path for required file
-    $classFile = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    $classFile = __DIR__.'/'.str_replace('\\', '/', $class).'.php';
 
     if (!file_exists($classFile) || !is_file($classFile)) {
         return;
