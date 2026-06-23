1ef7d2be287f103aa5546619df70131725777135
varun kewlani <varun.kewlani322@webkul.in>
2026-06-23 11:22:28 +0530

    Deleted: code form menu tpl file and adminstatstab as that has been present now in seperate PR
    Co-authored-by: Name <sumit201@webkul.in>

--- admin/themes/default/template/controllers/stats/menu.tpl
+++ admin/themes/default/template/controllers/stats/menu.tpl
@@ -28,14 +28,7 @@
 		{if count($modules)}
 			{foreach $modules as $module}
 				{if $module_instance[$module.name]}
-					{if isset($module_tabs[$module.name]) && $module_tabs[$module.name]}
-						{foreach $module_tabs[$module.name] as $tab}
-							{assign var="tab_active" value=($current_module_name == $module.name && $current_tab == $tab.key)}
-							<a class="list-group-item{if $tab_active} active{/if}" href="{$current|escape:'html':'UTF-8'}&amp;token={$token|escape:'html':'UTF-8'}&amp;module={$module.name|escape:'html':'UTF-8'}&amp;tab={$tab.key|escape:'html':'UTF-8'}">{$tab.label}</a>
-						{/foreach}
-					{else}
-						<a class="list-group-item{if ($current_module_name && $current_module_name == $module.name)} active{/if}" href="{$current|escape:'html':'UTF-8'}&amp;token={$token|escape:'html':'UTF-8'}&amp;module={$module.name}">{$module_instance[$module.name]->displayName}</a>
-					{/if}
+					<a class="list-group-item{if ($current_module_name && $current_module_name == $module.name)} active{/if}" href="{$current|escape:'html':'UTF-8'}&amp;token={$token|escape:'html':'UTF-8'}&amp;module={$module.name}">{$module_instance[$module.name]->displayName}</a>
 				{/if}
 			{/foreach}
 		{else}
--- controllers/admin/AdminStatsTabController.php
+++ controllers/admin/AdminStatsTabController.php
@@ -104,7 +104,6 @@ abstract class AdminStatsTabControllerCore extends AdminPreferencesControllerCor
         $action .= ($module ? '&module='.Tools::safeOutput($module) : '');
         $action .= (($id_product = Tools::getValue('id_product')) ? '&id_product='.Tools::safeOutput($id_product) : '');
         $action .= (($id_hotel = Tools::getValue('id_hotel')) ? '&id_hotel='.Tools::safeOutput($id_hotel) : '');
-        $action .= (($tab = Tools::getValue('tab')) ? '&tab='.Tools::safeOutput($tab) : '');
         $tpl->assign(array(
             'current' => self::$currentIndex,
             'token' => $token,
@@ -152,13 +151,9 @@ abstract class AdminStatsTabControllerCore extends AdminPreferencesControllerCor
 
         $modules = $this->getModules();
         $module_instance = array();
-        $module_tabs = array();
         foreach ($modules as $m => $module) {
             if ($module_instance[$module['name']] = Module::getInstanceByName($module['name'])) {
                 $modules[$m]['displayName'] = $module_instance[$module['name']]->displayName;
-                if (method_exists($module_instance[$module['name']], 'getStatsTabs')) {
-                    $module_tabs[$module['name']] = $module_instance[$module['name']]->getStatsTabs();
-                }
             } else {
                 unset($module_instance[$module['name']]);
                 unset($modules[$m]);
@@ -170,11 +165,9 @@ abstract class AdminStatsTabControllerCore extends AdminPreferencesControllerCor
         $tpl->assign(array(
             'current' => self::$currentIndex,
             'current_module_name' => Tools::getValue('module', 'statsforecast'),
-            'current_tab' => Tools::getValue('tab', ''),
             'token' => $this->token,
             'modules' => $modules,
-            'module_instance' => $module_instance,
-            'module_tabs' => $module_tabs,
+            'module_instance' => $module_instance
         ));
 
         return $tpl->fetch();
