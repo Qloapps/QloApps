dd35ba6879a8a7a2d5be3a72881d7e8bc2904601
varun kewlani <varun.kewlani322@webkul.in>
2026-08-12 11:46:20 +0530

    Updated: Docs folder
    Co-authored-by: Name <sumit201@webkul.in>

--- docs/licences/index.php
+++ docs/licences/index.php
@@ -2,10 +2,10 @@
 /**
 * NOTICE OF LICENSE
 *
-* This source file is subject to the Academic Free License (AFL 3.0)
+* This source file is subject to the Open Software License version 3.0
 * that is bundled with this package in the file LICENSE.md
 * It is also available through the world-wide-web at this URL:
-* https://opensource.org/licenses/afl-3.0.php
+* https://opensource.org/license/osl-3-0-php
 * If you did not receive a copy of the license and are unable to
 * obtain it through the world-wide-web, please send an email
 * to support@qloapps.com so we can send you a copy immediately.
@@ -18,7 +18,7 @@
 *
 * @author Webkul IN
 * @copyright Since 2010 Webkul
-* @license https://opensource.org/licenses/afl-3.0.php Academic Free License 3.0
+* @license https://opensource.org/license/osl-3-0-php Open Software License version 3.0
 */
 
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
