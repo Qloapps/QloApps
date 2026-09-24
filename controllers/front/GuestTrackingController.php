ba8ae8b24d64a0db1ff010437aa8f0ffffb7f903
amit sharma <amit.sharma322@webkul.in>
2026-09-24 13:06:27 +0530

    Fixed: Resolve fatal error on front office order page after deleting rooms

--- controllers/front/GuestTrackingController.php
+++ controllers/front/GuestTrackingController.php
@@ -280,6 +280,12 @@ class GuestTrackingControllerCore extends FrontController
                                 $cartHotelData[$type_key]['id_product'] = $type_value['product_id'];
                                 $cartHotelData[$type_key]['cover_img'] = $cover_img;
 
+                                // Prefer the name stored at order-creation time so it stays historically accurate
+                                if (!empty($order_bk_data[0]['selling_object_name'])) {
+                                    $cartHotelData[$type_key]['selling_object_name'] = $order_bk_data[0]['selling_object_name'];
+                                    $cartHotelData[$type_key]['selling_object_plural_name'] = $order_bk_data[0]['selling_object_plural_name'];
+                                }
+
                                 foreach ($order_bk_data as $data_k => $data_v) {
                                     $date_join = strtotime($data_v['date_from']).strtotime($data_v['date_to']);
 
@@ -538,6 +544,7 @@ class GuestTrackingControllerCore extends FrontController
                 $order->obj_hotel_branch_information = $objHotelBranchInformation;
                 $order->hotel_address_info = $hotelAddressInfo;
                 $order->hotel_refund_rules = $hotelRefundRules;
+                $order->property_type = $objHotelBranchInformation ? $objHotelBranchInformation->propertyTypeName : null;
                 //end
 
                 Hook::exec('actionOrderDetail', array('carrier' => $order->carrier, 'order' => $order));
--- themes/hotel-reservation-theme/guest-tracking.tpl
+++ themes/hotel-reservation-theme/guest-tracking.tpl
@@ -63,6 +63,7 @@
 			{assign var=obj_hotel_branch_information value=$order->obj_hotel_branch_information}
 			{assign var=hotel_address_info value=$order->hotel_address_info}
 			{assign var=hotel_refund_rules value=$order->hotel_refund_rules}
+			{assign var=property_type value=$order->property_type}
 
 			{if isset($order->total_old)}
 				{assign var=total_old value=$order->total_old}
