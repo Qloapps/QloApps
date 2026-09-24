a891f4e4f50e496664493373e800983bbd7ac26b
amit sharma <amit.sharma322@webkul.in>
2026-09-24 18:51:01 +0530

    Fixed: Resolve notice when changing status of a canceled order
    Co-authored-by: Name <sumit201@webkul.in>

--- classes/Tools.php
+++ classes/Tools.php
@@ -1885,6 +1885,8 @@ class ToolsCore
 
     public static function math_round($value, $places, $mode = PS_ROUND_HALF_UP)
     {
+        $value = (float) $value;
+
         //If PHP_ROUND_HALF_UP exist (PHP 5.3) use it and pass correct mode value (PrestaShop define - 1)
         if (defined('PHP_ROUND_HALF_UP')) {
             return round($value, $places, $mode - 1);
